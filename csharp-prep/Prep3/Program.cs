4131ae8e99e49074700352cdfffddd666194d9f8
patrickvirina <patrickvirina@gmail.com>
2024-04-25 23:49:18 +0800

    c# prep 3 assignment added. Tried doing the stretch challenge.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Core Requirement #1 and #2
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        
+        // Core Requirement #3
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        //Stretch challenge
+        int guesses  = 0;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            //Stretch challenge
+            guesses++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it in {guesses} times!");
+            }
+
+
+        }       
+
     }
 }
