eb978b3f4c01a0365f6e603b1bc764586e66db68
patrickvirina <patrickvirina@gmail.com>
2024-04-26 01:09:17 +0800

    c# prep 4 assignment added.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int input;
+        do
+        {
+            Console.Write("Enter number: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (input != 0)
+                numbers.Add(input);
+
+        } while (input != 0);
+
+        if (numbers.Count > 0)
+        {
+            // Core Requirements #1, #2, and #3
+            int sum = numbers.Sum();
+            double average = numbers.Average();
+            int max = numbers.Max();
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers entered.");
+        }
     }
 }
