55ffa871d7898c19decd8a90bb34facd58fdc2ec
patrickvirina <patrickvirina@gmail.com>
2024-04-30 21:44:54 +0800

    Learning 02 prep assignment added

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+     Job job1 = new Job();
+        job1._jobTitle = "SEO Specialist";
+        job1._company = "No Joke Marketing";
+        job1._startYear = 2018;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "SEO and Content Specialist";
+        job2._company = "Clean Marketing";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "Patrick Viriña";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
